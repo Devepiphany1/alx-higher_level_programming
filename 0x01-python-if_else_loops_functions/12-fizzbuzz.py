24867d412babe298eb01a894f045d4d3cc35b527
Devepiphany1 <franklinanthony89@gmail.com>
2024-05-10 15:11:52 +0000

    Updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,6 @@
-n range(1, 101):
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end="")
         elif num % 5 == 0:
